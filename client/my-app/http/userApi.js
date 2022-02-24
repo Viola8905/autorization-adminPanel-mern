94c605d92fa1138f7dfac813157a9c1ce74b2bd1
Viola8905 <viola89055@gmail.com>
2022-02-24 22:19:36 +0200

    created http folder in client

---
+++ client/my-app/http/userApi.js
@@ -0,0 +1,6 @@
+import { $authHost, $host } from "./index";
+
+export const registration = async (email,password) => {
+	const response = await $host.post('api/auth/registration',{email, password, role:'ADMIN'});
+	return response
+}
