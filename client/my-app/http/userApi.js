4975152c35483c979066ca894c392af0f2537737
Viola Prokopchuk <74563100+Viola8905@users.noreply.github.com>
2022-04-20 16:14:35 +0300

    Delete client/my-app directory

--- client/my-app/http/userApi.js
+++
@@ -1,6 +0,0 @@
-import { $authHost, $host } from "./index";
-
-export const registration = async (email,password) => {
-	const response = await $host.post('api/auth/registration',{email, password, role:'ADMIN'});
-	return response
-}
--- client/my-app/src/components/input/Input.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Input = (props) => {
-	return (
-		<input onChange = {(event)=> props.setValue(event.target.value)} value = {props.value} type = {props.type} placeholder={props.placeholder}/>
-	)
-}
-
-export default Input
